19fe8c82e60ff37c5c3db21a6237fc18c55418dd
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2024-01-03 15:00:23 +0100

    Update main.tf
    add -d for detache

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "null_resource" "clone_and_run" {
     command = <<EOF
     git clone https://github.com/rosenthalpreston/cp4---Mise-en-production-ITSM-GLPI-10.git
     cd cp4---Mise-en-production-ITSM-GLPI-10
-    docker-compose up
+    docker-compose up -d
     EOF
   }
 }

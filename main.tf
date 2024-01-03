721d336b42fb0aadd5b8269d9c5d304b69c61052
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2024-01-03 13:03:47 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "local" {
+  version = "~> 1.4"
+}
+
+resource "null_resource" "clone_and_run" {
+  provisioner "local-exec" {
+    command = <<EOF
+    git clone https://github.com/rosenthalpreston/cp4---Mise-en-production-ITSM-GLPI-10.git
+    cd cp4---Mise-en-production-ITSM-GLPI-10
+    docker-compose up
+    EOF
+  }
+}
